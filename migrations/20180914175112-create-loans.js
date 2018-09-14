d7dd2bd52050aeced503400fbd6b9355b71b4269
LeeVaughn <dlvprogramming@gmail.com>
2018-09-14 13:23:02 -0700

    updated properties for Loans table

--- migrations/20180914175112-create-loans.js
+++ migrations/20180914175112-create-loans.js
@@ -18,13 +18,13 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       loaned_on: {
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       return_by: {
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       returned_on: {
-        type: Sequelize.DATE
+        type: Sequelize.DATEONLY
       },
       createdAt: {
         allowNull: false,
--- models/loans.js
+++ models/loans.js
@@ -1,13 +1,61 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Loans = sequelize.define('Loans', {
-    id: DataTypes.INTEGER,
-    book_id: DataTypes.INTEGER,
-    patron_id: DataTypes.INTEGER,
-    loaned_on: DataTypes.DATE,
-    return_by: DataTypes.DATE,
-    returned_on: DataTypes.DATE
-  }, {});
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true
+    },
+    book_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          msg: "Book ID should be a number"
+        },
+        notEmpty: {
+          msg: "Book ID cannot be left blank"
+        }
+      }
+    },
+    patron_id: {
+      type: DataTypes.INTEGER,
+      validate: {
+        isInt: {
+          msg: "Patron ID should be a number"
+        },
+        notEmpty: {
+          msg: "Patron ID cannot be left blank"
+        }
+      }
+    },
+    loaned_on: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isDate: {
+          msg: "Loaned On must be a valid date"
+        }
+      }
+    },
+    return_by: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isDate: {
+          msg: "Return By must be a valid date"
+        }
+      }
+    },
+    returned_on: {
+      type: DataTypes.DATEONLY,
+      validate: {
+        isDate: {
+          msg: "Returned On must be a valid date"
+        }
+      }
+    }
+  }, {
+    timestamps: false
+  });
   loans.associate = function(models) {
     // associations can be defined here
   };
